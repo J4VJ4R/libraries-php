177faf0cd424603b8ea2b14a95ba269b16dfc104
javi <ingenierodesoftware73@gmail.com>
2023-04-06 19:10:01 -0500

    html2pdf solo para php 7.4

--- Libs/generator-pdf/index.php
+++ Libs/generator-pdf/index.php
@@ -6,8 +6,9 @@ use Spipu\Html2Pdf\Html2Pdf;
 
 $html2pdf = new Html2Pdf();
 
-$html = "<p>Hola mundo desde una librería php</p>";
-$html .= "<p>Este es un párrafo</p>";
+ob_start();
+require_once 'pdf_html.php';
+$html = ob_get_clean();
 
 $html2pdf->writeHTML($html);
 $html2pdf->output('pdf_generado.pdf');
